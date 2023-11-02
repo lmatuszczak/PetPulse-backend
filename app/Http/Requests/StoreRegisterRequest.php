aea024850e026a93129bc457464d6a32d1afc36c
Łukasz Matuszczak <lukasz.matuszczak.yes@gmail.com>
2023-11-02 15:23:57 +0100

    5-be-registration

---
+++ app/Http/Requests/StoreRegisterRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    /*public function authorize(): bool
+    {
+        return true;
+    }*/
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => 'required|unique:users|email|max:255',
+            'name' => 'required|unique:users|max:255',
+            'password' => 'required|max:255|min:8|regex:/^.*(?=.{3,})(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[\d\x])(?=.*[!$#%]).*$/',
+            'passwordCheck' => 'required|max:255|same:password',
+        ];
+    }
+}
