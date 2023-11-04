cd7e904f5a1bd529a15f913d56e35380828bf8ba
Łukasz Matuszczak <lukasz.matuszczak.yes@gmail.com>
2023-11-05 00:00:34 +0100

    11-be-animalowner

---
+++ database/seeders/AnimalSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Animal;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AnimalSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Animal::create(
+            [
+                'name' => 'Luna',
+                'microchip_number' => 123456789012345,
+                'weight' => 50,
+                'age' => 5,
+                'color' => 'black',
+                'gender' => 'XYZ',
+                'species' => 'mammal',
+                'breed_id' => 1,
+                'owner_id' => 1,
+            ]
+        );
+    }
+}
